21e159dc96b83c63e59c37d7d91ae928d4215da3
Kafil Nasdami (from Travis CI) <kafil.nasdami@gmail.com>
2020-04-18 02:41:20 +0000

    Deploy Kafiil/covid-app to github.com/Kafiil/covid-app.git:gh-pages

---
+++ precache-manifest.e7a8ca25fdab5f4c0e249ed15a01ee57.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f66d6a8eaf0b80635394b6ecbe1b6239",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07b358e169b869e54b4d",
+    "url": "/static/css/main.292fa60a.chunk.css"
+  },
+  {
+    "revision": "9b9381af69c3f9805fd7",
+    "url": "/static/js/2.6cc174c4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6cc174c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07b358e169b869e54b4d",
+    "url": "/static/js/main.bc05646a.chunk.js"
+  },
+  {
+    "revision": "d3be38e7f455fb7c073c",
+    "url": "/static/js/runtime-main.25917a94.js"
+  },
+  {
+    "revision": "69cfee0a0104e045be345c0eaf13afb6",
+    "url": "/static/media/confirmed.69cfee0a.svg"
+  },
+  {
+    "revision": "eab55e676bb986c6b4bdc1c47d055607",
+    "url": "/static/media/deaths.eab55e67.svg"
+  },
+  {
+    "revision": "527b2c7f5e05feb89144a94db59bb7ed",
+    "url": "/static/media/negatives.527b2c7f.svg"
+  },
+  {
+    "revision": "c542485721b1cdd858b18d28eda9eb1f",
+    "url": "/static/media/recovered.c5424857.svg"
+  }
+]);
